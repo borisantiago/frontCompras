9007b034c0f3bb69a247331ce90386a07a519d28
boris <boris96bs7@gmail.com>
2021-02-12 04:15:56 -0500

    Implementacion de imagenes

--- src/main/resources/static/js/funcionesArticulos.js
+++ src/main/resources/static/js/funcionesArticulos.js
@@ -1,2 +1,23 @@
 //llamado tabla articulos
 $("#tablaArticulos").load("js/tablaArticulos.html");
+
+
+function eliminarProducto(id){
+	if(confirm("Esta seguro de eliminar el gasto?")){     
+            $.ajax({
+                type:"DELETE",
+                url:"http://localhost:8090/deleteid/" + id,
+                data:"id="+id, 
+	
+		});
+		
+		$("#eliminado").show("fast");
+            
+			//alerta de eliminar quitando en un tiempo
+            setTimeout(function() {
+                $("#eliminado").fadeOut();           
+            },3500);
+
+			$("#"+id).hide("slow");
+		}
+}
